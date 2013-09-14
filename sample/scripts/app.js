c48179dc24cbb53133d5f9d5fc907394134ab220
Sebastian de Castelberg <sebu@kpricorn.org>
2013-09-14 05:03:34 +0200

    implement backbone model/view app

--- sample/scripts/app.js
+++ sample/scripts/app.js
@@ -1,54 +1,19 @@
-define(['lodash', 'paper', 'game-of-life'], function(_, paper, Game) {
-  window.Game = Game.Game;
-  window.paper = paper
-  window.canvas = document.getElementById('gol');
-  paper.setup(canvas);
-
-  var game = new Game.Game();
-  //game.field['0/0'] = [0, 0];
-  //game.field['0/1'] = [0, 1];
-  //game.field['0/2'] = [0, 2];
-
-  // Beacon
-  //game.field['0/0'] = [0, 0];
-  //game.field['0/1'] = [0, 1];
-  //game.field['1/0'] = [1, 0];
-  //game.field['1/1'] = [1, 1];
-  //game.field['2/2'] = [2, 2];
-  //game.field['2/3'] = [2, 3];
-  //game.field['3/2'] = [3, 2];
-  //game.field['3/3'] = [3, 3];
-
-  // glider
-  game.field['0/0'] = [0, 0];
-  game.field['1/0'] = [1, 0];
-  game.field['1/1'] = [1, 1];
-  game.field['1/-1'] = [1, -1];
-  game.field['2/-1'] = [2, -1];
-
-  xOffset = canvas.width / 2;
-  yOffset = canvas.height / 2;
-  var rects = [];
-
-  var tick = function() {
-    var topLeft;
-    var rect;
-    var x;
-    var y;
-    var rect;
-    _.invoke(rects, 'remove');
-    rects = _.map(game.cells(), function(c) {
-      x = c[0] + xOffset;
-      y = c[1] + yOffset;
-      topLeft = new paper.Point(x, y);
-      rect = new paper.Path.Rectangle(topLeft, [1, 1]);
-      rect.fillColor = 'red';
-      return rect;
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/gol_view',
+  'models/gol_model',
+], function($, _, Backbone, GOLView, GOLModel) {
+  var initialize = function(){
+    var golView = new GOLView({
+      model: new GOLModel()
     });
-    paper.view.draw();
-    game.turn();
-    setTimeout(tick, 1)
+    golView.render();
+    Backbone.history.start();
   };
 
-  tick();
+  return {
+    initialize: initialize
+  };
 });
--- sample/scripts/main.js
+++ sample/scripts/main.js
@@ -3,9 +3,16 @@ require.config({
   urlArgs: "bust=" + (new Date()).getTime(),
   paths: {
     lodash: 'vendor/lodash/dist/lodash.compat',
+    jquery: 'vendor/jquery/jquery',
+    underscore: 'vendor/underscore-amd/underscore',
     paper: 'vendor/paper/dist/paper',
-    'game-of-life': 'vendor/game-of-life/dist/game-of-life'
+    'game-of-life': 'vendor/game-of-life/dist/game-of-life',
+    backbone: 'vendor/backbone-amd/backbone'
   }
 });
 
-require(['app']);
+require(['app'], function(App){
+  $(function() {
+    App.initialize();
+  });
+});
